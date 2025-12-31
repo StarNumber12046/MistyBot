bac908f2709b6b7f50c442c9b3614e3d762e0304
StarNumber12046 <starnumber.official12046@gmail.com>
2025-12-31 20:19:09 +0100

    Added user info command

---
+++ src/commands/utils/user.ts
@@ -0,0 +1,43 @@
+import {
+  SlashCommandBuilder,
+  type ChatInputCommandInteraction,
+  MessageFlags,
+} from "discord.js";
+import { env } from "process";
+import { ratelimit, redis } from "../../utils/redis.js";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("The user to see info about")
+        .setRequired(true)
+    )
+    .setDescription("Command to see info about a user"),
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.user.id !== env.OWNER_ID) return;
+    const user = interaction.options.getUser("user");
+    if (!user) {
+      await interaction.reply({
+        content: "Invalid user",
+        flags: MessageFlags.Ephemeral,
+      });
+      return;
+    }
+    const { remaining, reset } = await ratelimit.getRemaining(user.id);
+    const blacklisted = await redis.get(`blacklist:${user.id}`);
+    const preferredModel = await redis.get(`user:${user.id}:model`);
+    await interaction.reply({
+      content: `${
+        user.tag
+      }'s profile:\nRemaining messages: ${remaining}\nResets in: <t:${Math.floor(
+        reset / 1000
+      )}:R>\nIs blacklisted: ${
+        blacklisted ? " true" : " false"
+      }\nPreferred model: ${preferredModel ?? "default"}`,
+      flags: MessageFlags.Ephemeral,
+    });
+  },
+};
